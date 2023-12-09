66a1d7ed88df213b19b1ad5ef4110bdae7906cdf
jyothika28 <jyothikajalla032@gmail.com>
2023-12-09 16:02:44 -0500

    added events and tickets

---
+++ events/dao.js
@@ -0,0 +1,25 @@
+import EventModel from "./model.js";
+
+export const createEvent = async (event) => {
+    return await EventModel.create(event);
+}
+
+export const findAllEvents = async () => {
+    return await EventModel.find();
+}
+
+export const findEventById = async (eventId) => {
+    return await EventModel.findById(eventId);
+}
+
+export const updateEvent = async (eventId, event) => {
+    return await EventModel.updateOne({_id: eventId}, {$set: event})
+}
+
+export const deleteEvent = async (eventId) => {
+    return await EventModel.deleteOne({_id: eventId})
+}
+
+export const searchEvents = async (query) => {
+    return await EventModel.find({$text: {$search: query}})
+}
